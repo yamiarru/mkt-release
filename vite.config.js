b958e999cd6e58c7fe477e794714b749e73d3df7
Yamila <69605571+yamiarru@users.noreply.github.com>
2024-10-20 00:41:20 -0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mkt-release/', //repo github
+  // base: '/mkt-release/', //repo github
 })
